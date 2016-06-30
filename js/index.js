e27866e1c195d5968e50f6c27a2f81db87d53639
FatGuy808 <jefferybob145@gmail.com>
2016-06-29 14:26:41 -1000

    Hi
    Football Game

---
+++ js/index.js
@@ -0,0 +1,15 @@
+var Teams = ["Seahawks", "Patriots", "NY Giants", "LA Rams", "Dallas Cowboys", 
+"Philly Eagles", "Cleavland Brown", "Colts", "SD Chargers", "Oakland Raiders",
+"Chicago Bears", "GB Packers", "Detroit Lions", "Pitt Steelers", "Houston Texans",
+"SF 49ers", "Jacksonville Jaguars", "Wasinghton Redskins", "Carolina Panthers",
+"Minn Vikings", "Baltimore Ravens", "NYJ", "Cinn Bengals", "Miami Dolphins", "Buff Bills",
+"Ari Cardinals", "Tampa Bucs", "Atlanta Falcons", "KC Chiefs", "NO Saints", "Tenn Titans"]
+
+function pickTeam(){
+	var teamNumber = Math.floor(Math.random()*Teams.length);
+	var teamNumber2 = Math.floor(Math.random()
+	var team1Score = 0;
+	var team2Score = 0;
+
+	document. getElementById('Football Teams') 
+}
