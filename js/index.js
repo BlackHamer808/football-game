7088449e4df4c1a591e53d5afd583577985319e7
BlackHamer808 <dmobley808@gmail.com>
2016-07-06 14:30:13 -1000

    dod
    dkdk

--- js/index.js
+++ js/index.js
@@ -1,25 +1,27 @@
-var Teams = ["Seahawks", "Patriots", "NY Giants", "LA Rams", "Dallas Cowboys", 
+var Teams = ["Seahawks  ", "Patriots", "NY Giants", "LA Rams", "Dallas Cowboys", 
 "Philly Eagles", "Cleavland Brown", "Colts", "SD Chargers", "Oakland Raiders",
 "Chicago Bears", "GB Packers", "Detroit Lions", "Pitt Steelers", "Houston Texans",
 "SF 49ers", "Jacksonville Jaguars", "Wasinghton Redskins", "Carolina Panthers",
 "Minn Vikings", "Baltimore Ravens", "NYJ", "Cinn Bengals", "Miami Dolphins", "Buff Bills",
-"Ari Cardinals", "Tampa Bucs", "Atlanta Falcons", "KC Chiefs", "NO Saints", "Tenn Titans," "Denver Broncows"]
+"Ari Cardinals", "Tampa Bucs", "Atlanta Falcons", "KC Chiefs", "NO Saints", "Tenn Titans", "Denver Broncows"]
 
-function pickTeam(){
+function see teams(){
 	var teamNumber = Math.floor(Math.random()*Teams.length);
 	var teamNumber2 = Math.floor(Math.random()*Teams.length);
-	var team1Score = Math.floor(Math.random()*70);
-	var team2Score = Math.floor(Math.random()*70);
-
 	document.getElementById('FootballTeams').innerHTML = 
 	Teams[teamNumber] + ": " + team1Score + " vs. " +
 	Teams[teamNumber2] + ": " + team2Score;
+     }
 
-	if (team1Score > team2Score) {
+ }
+ function see score {var team1Score = Math.floor(Math.random()*70);
+	var team2Score = Math.floor(Math.random()*70);
+if (team1Score > team2Score) {
 		document.getElementById('winner').innerHTML = Teams[teamNumber] + " wins";
 	};
 	if (team2Score > team1Score) {
 		document.getElementById('winner').innerHTML = Teams[teamNumber2] + " wins";
-     }
+
 
  }
+
