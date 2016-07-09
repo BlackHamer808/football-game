6e2781c4a8b67d49588465afa5f9363a93dc492b
FatGuy808 <jefferybob145@gmail.com>
2016-07-08 14:30:48 -1000

    jdjd
    shsh

--- js/index.js
+++ js/index.js
@@ -3,17 +3,21 @@ var Teams = ["Seahawks  ", "Patriots", "NY Giants", "LA Rams", "Dallas Cowboys",
 "Chicago Bears", "GB Packers", "Detroit Lions", "Pitt Steelers", "Houston Texans",
 "SF 49ers", "Jacksonville Jaguars", "Wasinghton Redskins", "Carolina Panthers",
 "Minn Vikings", "Baltimore Ravens", "NYJ", "Cinn Bengals", "Miami Dolphins", "Buff Bills",
-"Ari Cardinals", "Tampa Bucs", "Atlanta Falcons", "KC Chiefs", "NO Saints", "Tenn Titans", "Denver Broncows"]
+"Ari Cardinals", "Tampa Bucs", "Atlanta Falcons", "KC Chiefs", "NO Saints", "Tenn Titans", 
+"Denver Broncos"]
 
 var teamNumber;
 var teamNumber2;
 var team1Score;
 var team2Score;
-
+var userRecord = [];
+ var userchoice;
+var winingteam;
 function seeteams(){
 	teamNumber = Math.floor(Math.random()*Teams.length);
 	teamNumber2 = Math.floor(Math.random()*Teams.length);
-	document.getElementById("FootballTeams").innerHTML =  Teams[teamNumber]+" vs. "+Teams[teamNumber2];
+	document.getElementById("FootballTeams").innerHTML =  
+	Teams[teamNumber]+" vs. "+Teams[teamNumber2];
 }
 
 
@@ -23,17 +27,22 @@ function seescore() {
 
 	if (team1Score > team2Score) {
 		document.getElementById('winner').innerHTML = Teams[teamNumber] + " wins";
-	};
-	
+		if (teamNumber==userchoice){
+			userRecord.push("win")
+		} else {
+			userRecord.push("lose")
+		}
+	}
 	if (team2Score > team1Score) {
 		document.getElementById('winner').innerHTML = Teams[teamNumber2] + " wins";
 	}
- 	document.getElementById('FootballTeams').innerHTML = 
-		Teams[teamNumber] + ": " + team1Score + " vs. " +
-		Teams[teamNumber2] + ": " + team2Score;
-
-
+ 	document.getElementById('FootballTeams').innerHTML =
+ 	 Teams[teamNumber] + ": " + team1Score + " vs. " + Teams[teamNumber2] + ": " + team2Score;
  }
 
-
-
+function guessOne (){
+	 userchoice = teamNumber  
+} 
+function guessTwo (){
+	userchoice = teamNumber2
+}
