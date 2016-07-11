714c0afbbea346ea1d26b24a5699769bc19456a3
FatGuy808 <jefferybob145@gmail.com>
2016-07-11 12:53:20 -1000

    untangled merges
    untangle merges

--- js/index.js
+++ js/index.js
@@ -3,13 +3,12 @@ var Teams = ["Seahawks  ", "Patriots", "NY Giants", "LA Rams", "Dallas Cowboys",
 "Chicago Bears", "GB Packers", "Detroit Lions", "Pitt Steelers", "Houston Texans",
 "SF 49ers", "Jacksonville Jaguars", "Wasinghton Redskins", "Carolina Panthers",
 "Minn Vikings", "Baltimore Ravens", "NYJ", "Cinn Bengals", "Miami Dolphins", "Buff Bills",
-<<<<<<< HEAD
+
 "Ari Cardinals", "Tampa Bucs", "Atlanta Falcons", "KC Chiefs", "NO Saints", "Tenn Titans", 
 "Denver Broncos"]
-=======
-<<<<<<< HEAD
-"Ari Cardinals", "Tampa Bucs", "Atlanta Falcons", "KC Chiefs", "NO Saints", "Tenn Titans", "Denver Broncows"]
->>>>>>> origin/master
+
+
+
 
 var teamNumber;
 var teamNumber2;
@@ -29,9 +28,7 @@ function seeteams(){
 function seescore() {
 	team1Score = Math.floor(Math.random()*70);
 	team2Score = Math.floor(Math.random()*70);
-=======
-"Ari Cardinals", "Tampa Bucs", "Atlanta Falcons", "KC Chiefs", "NO Saints", "Tennesse Titans",
-"Denver Broncos"]
+
 
 
 
@@ -45,7 +42,7 @@ function getscores(){
     	document.getElementById('FootballTeams').innerHTML = 
 	Teams[teamNumber] + ": " + team1Score + " vs. " +
 	Teams[teamNumber2] + ": " + team2Score;
->>>>>>> FatGuy808/master
+
 
 	if (team1Score > team2Score) {
 		document.getElementById('winner').innerHTML = Teams[teamNumber] + " wins";
@@ -56,32 +53,24 @@ function getscores(){
 		}
 	}
 	if (team2Score > team1Score) {
-<<<<<<< HEAD
+
 		document.getElementById('winner').innerHTML = Teams[teamNumber2] + " wins";
 	}
  	document.getElementById('FootballTeams').innerHTML =
  	 Teams[teamNumber] + ": " + team1Score + " vs. " + Teams[teamNumber2] + ": " + team2Score;
  }
 
-<<<<<<< HEAD
 function guessOne (){
 	 userchoice = teamNumber  
 } 
 function guessTwo (){
 	userchoice = teamNumber2
 }
-=======
 
-
-=======
 		document.getElementById('winner').innerHTML = Teams[teamNumber2] + " wins";};
-}
 
 
 
 
-     
 
- 
->>>>>>> FatGuy808/master
->>>>>>> origin/master
+  
