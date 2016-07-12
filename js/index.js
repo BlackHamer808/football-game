762bde2daef5509c96a07f964ccdfbad767775e4
BlackHamer808 <dmobley808@gmail.com>
2016-07-11 14:27:27 -1000

    duke
    duke

--- js/index.js
+++ js/index.js
@@ -1,17 +1,20 @@
-var Teams = ["Seahawks  ", "Patriots", "NY Giants", "LA Rams", "Dallas Cowboys", 
+var Teams = [
+"Seahawks", "Patriots", "NY Giants", "LA Rams", "Dallas Cowboys", 
 "Philly Eagles", "Cleavland Brown", "Colts", "SD Chargers", "Oakland Raiders",
 "Chicago Bears", "GB Packers", "Detroit Lions", "Pitt Steelers", "Houston Texans",
 "SF 49ers", "Jacksonville Jaguars", "Wasinghton Redskins", "Carolina Panthers",
 "Minn Vikings", "Baltimore Ravens", "NYJ", "Cinn Bengals", "Miami Dolphins", "Buff Bills",
-"Ari Cardinals", "Tampa Bucs", "Atlanta Falcons", "KC Chiefs", "NO Saints", "Tenn Titans", "Denver Broncos"]
+"Ari Cardinals", "Tampa Bucs", "Atlanta Falcons", "KC Chiefs", "NO Saints", "Tenn Titans", "Denver Broncos"
+];
 
 var teamNumber;
 var teamNumber2;
 var team1Score;
 var team2Score;
 var userRecord = [];
- var userchoice;
+var userchoice;
 var winingteam;
+
 function seeteam(){
 	teamNumber = Math.floor(Math.random()*Teams.length);
 	teamNumber2 = Math.floor(Math.random()*Teams.length);
@@ -22,22 +25,12 @@ function seeteam(){
 
 
 function seescore(){
-	var teamNumber = Math.floor(Math.random(0
-	)*Teams.length);
+	var teamNumber = Math.floor(Math.random(0)*Teams.length);
 	var teamNumber2 = Math.floor(Math.random()*Teams.length);
 	var team1Score = Math.floor(Math.random()*70);
 	var team2Score = Math.floor(Math.random()*70);
-    	document.getElementById('FootballTeams').innerHTML = 
-	Teams[teamNumber] + ": " + team1Score + " vs. " +
-	Teams[teamNumber2] + ": " + team2Score;
-}
+    document.getElementById('FootballTeams').innerHTML = Teams[teamNumber] + ": " + team1Score + " vs. " +Teams[teamNumber2] + ": " + team2Score;
 
-function guessOne(){
-	 userchoice = teamNumber  
-} 
-function guessTwo(){
-	userchoice = teamNumber2
-	
 	if (team1Score > team2Score) {
 		document.getElementById('winner').innerHTML = Teams[teamNumber] + " wins";
 		if (teamNumber==userchoice){
@@ -48,11 +41,14 @@ function guessTwo(){
 		}
 	}
 	if (team2Score > team1Score) {
+		document.getElementById('winner').innerHTML = Teams[teamNumber2] + " wins"
 
-		document.getElementById('winner').innerHTML = Teams[teamNumber2] + " wins";
 	}
- 	document.getElementById('FootballTeams').innerHTML =
- 	 Teams[teamNumber] + ": " + team1Score + " vs. " + Teams[teamNumber2] + ": " + team2Score;
+}
 
-
+function guessOne(){
+	 userchoice = teamNumber 
+} 
+function guessTwo(){
+	userchoice = teamNumber2
 }
